cf5da13db752321821463145c3df3b563f89f59c
Mads Bergdal <mbe@dips.no>
2015-01-29 18:59:51 +0100

    First commit

---
+++ MyMovies/src/business/Movie.java
@@ -0,0 +1,21 @@
+package business;
+
+public class Movie {
+	
+	private it.jtomato.gson.Movie movie;
+	
+	public Movie(it.jtomato.gson.Movie movie) {
+		this.movie = movie;		
+	}
+	
+	public String getTitle(){
+		return this.movie.title;
+	}
+	
+	public String getId(){
+		return this.movie.id;
+	}
+	
+	
+	
+}
---
+++ MyMovies/src/tests/NetHttpClientMockUp.java
@@ -0,0 +1,29 @@
+package tests;
+
+import it.jtomato.net.NetHttpClientInterface;
+
+import java.util.HashMap;
+
+/**
+ * HTTP Client mockup used for testing purposes
+ * 
+ * @author <a href="mailto:tambug@gmail.com">Giordano Tamburrelli</a>
+ * 
+ * @version 1.0
+ **/
+public class NetHttpClientMockUp implements NetHttpClientInterface {
+
+	private String response = "{}";
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+	public String get(String url) {
+		return response;
+	}
+
+	public String buildUrl(String path, HashMap<String, String> params){
+		return "";
+	}
+}
